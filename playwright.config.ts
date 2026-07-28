2388cad3cb12f8a7807fc4ceb6fb37ca99984049
Aiden Bai <aiden@million.dev>
2026-07-27 17:49:50 -0700

    chore: simplify Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,22 +1,12 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
-  timeout: 20_000,
-  preserveOutput: "always",
   use: {
     baseURL: "http://127.0.0.1:5173",
   },
   webServer: {
     command: "bun run dev",
     url: "http://127.0.0.1:5173",
-    reuseExistingServer: !process.env.CI,
-    timeout: 120_000,
   },
-  projects: [
-    {
-      name: "chromium",
-      use: devices["Desktop Chrome"],
-    },
-  ],
 });
